7d899a6cdcaf14af3f633c0495ee9f177df1bdb9
mrshuhrat <shuhratadm1n@gmail.com>
2020-12-20 14:53:27 +0500

    inittial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import { useReduser, useState } from 'react'
+import Questions from './data';
 import './App.css';
 
 function App() {
+  const [enable, setEnable] = useState(false)
+
+  function buttonClick(evt) {
+    if (evt.target.attributes.iscorrect.value === 'true') {
+      evt.target.classList.add('true');
+    } else {
+      evt.target.classList.add('false')
+    }
+
+    for (let i = 0; i <= 2; i++) {
+      evt.target.parentNode.children[i].disabled = true
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <>
+        <h1 className="app-name">iTest</h1>
+        <ul className="question-list">
+
+          {
+            Questions.map((question, index) =>{
+              return (
+                <li className="question-item">
+                  <h3 className="question-text">{question.id}. {question.question}</h3>
+                  <div className="buttons-list" key={index}>
+                    {
+                      question.answers.map((answer, id) => (
+                        <button className="answer-btn" iscorrect={String(answer.isTrue)} disabled={enable} onClick={buttonClick} key={id} type="button">{answer.option}) {answer.answerText}</button> 
+                      ))                          
+                    }
+                  </div>
+                </li>
+              )
+            })
+          }
+
+        </ul>      
+      </>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,89 @@
+const questions = [
+  {
+    id: "1",
+    question: "Question 1?",
+    answers: [
+      {
+        answerText: "answer1",
+        isTrue: true,
+        option: "A"
+      },
+      {
+        answerText: "answer2",
+        isTrue: false,
+        option: "B"
+      },
+      {
+        answerText: "answer3",
+        isTrue: false,
+        option: "D"
+      }
+    ]
+  },
+  {
+    id: "2",
+    question: "Question 2?",
+    answers: [
+      {
+        answerText: "answer1",
+        isTrue: true,
+        option: "A"
+      },
+      {
+        answerText: "answer2",
+        isTrue: false,
+        option: "B"
+      },
+      {
+        answerText: "answer3",
+        isTrue: false,
+        option: "D"
+      }
+    ]
+  },
+  {
+    id: "3",
+    question: "Question 3?",
+    answers: [
+      {
+        answerText: "answer1",
+        isTrue: true,
+        option: "A"
+      },
+      {
+        answerText: "answer2",
+        isTrue: false,
+        option: "B"
+      },
+      {
+        answerText: "answer3",
+        isTrue: false,
+        option: "D"
+      }
+    ]
+  },
+  
+  {
+    id: "4",
+    question: "Question 4?",
+    answers: [
+      {
+        answerText: "answer1",
+        isTrue: true,
+        option: "A"
+      },
+      {
+        answerText: "answer2",
+        isTrue: false,
+        option: "B"
+      },
+      {
+        answerText: "answer3",
+        isTrue: false,
+        option: "D"
+      }
+    ]
+  },
+]
+
+export default questions
